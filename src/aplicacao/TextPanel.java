f97032b8e219eea1236719bc6f58595ae27812ff
FabiDiasL <fabi_lima_@hotmail.com>
2023-02-15 16:20:47 -0300

    update

---
+++ src/aplicacao/TextPanel.java
@@ -0,0 +1,25 @@
+package aplicacao;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class TextPanel extends JPanel{
+	
+	private JTextField txtNumero;
+	
+	public TextPanel() {
+		setLayout(new FlowLayout());
+		
+		txtNumero = new JTextField(25);
+		txtNumero.setHorizontalAlignment(JTextField.RIGHT);
+		txtNumero.setEnabled(false);
+		
+		add(txtNumero);
+	}
+
+	public JTextField getTxtNumero() {
+		return txtNumero;
+	}	
+}
