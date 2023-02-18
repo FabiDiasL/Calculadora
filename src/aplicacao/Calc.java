196ff1d796f9fb1bd7f702f1494eafcf7d22e413
FabiDiasL <fabi_lima_@hotmail.com>
2023-02-18 19:06:31 -0300

    update

--- src/aplicacao/Calc.java
+++ src/aplicacao/Calc.java
@@ -6,34 +6,32 @@ import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 public class Calc extends JFrame {
-	
+
 	public Calc() throws Exception {
 		super("Calculadora");
-		
+
 		UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(300, 350);
 		setLocation(400, 200);
-		
+
 		setLayout(new BorderLayout());
-		
+
 		TextPanel textPanel = new TextPanel();
 		add(BorderLayout.NORTH, textPanel);
-		
+
 		JPanel digitosPanel = new JPanel();
 		digitosPanel.setLayout(new BorderLayout());
 		digitosPanel.add(BorderLayout.CENTER, new NumerosPanel(textPanel.getTxtNumero()));
 		digitosPanel.add(BorderLayout.EAST, new OperacoesPanel(textPanel.getTxtNumero()));
-				
+
 		add(BorderLayout.CENTER, digitosPanel);
-		
+
 		setVisible(true);
 	}
-	
-	public static void main(String[] args) throws Exception {
-		new Calc(); 
-	}
-	
 
+	public static void main(String[] args) throws Exception {
+		new Calc();
+	}
 }
--- src/aplicacao/OperacoesPanel.java
+++ src/aplicacao/OperacoesPanel.java
@@ -8,49 +8,94 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class OperacoesPanel extends JPanel implements ActionListener{
-	
+public class OperacoesPanel extends JPanel implements ActionListener {
+
 	private JTextField txtText;
-	
-	private JButton botaosomar;
-	private JButton botaosubtrair;
-	private JButton botaomultiplicar;
-	private JButton botaodividir;
-	private JButton botaoigual;
+
+	private char operacao;
+	private double valor1;
+	private double valor2;
+
+	private JButton botaoSomar;
+	private JButton botaoSubtrair;
+	private JButton botaoMultiplicar;
+	private JButton botaoDividir;
+	private JButton botaoIgual;
 	private JButton botaoLimpar;
-	
+
 	public OperacoesPanel(JTextField txtText) {
 		this.txtText = txtText;
-		
+
 		setLayout(new GridLayout(3, 2));
-		
-		botaosomar = new JButton("+");
-		botaosomar.addActionListener(this);
-		add(botaosomar);
-		
-		botaosubtrair = new JButton("-");
-		botaosubtrair.addActionListener(this);
-		add(botaosubtrair);
-		
-		botaomultiplicar = new JButton("*");
-		botaomultiplicar.addActionListener(this);
-		add(botaomultiplicar);
-		
-		botaodividir = new JButton("/");
-		botaodividir.addActionListener(this);
-		add(botaodividir);
-		
-		botaoigual = new JButton("=");
-		botaoigual.addActionListener(this);
-		add(botaoigual);
-		
+
+		botaoSomar = new JButton("+");
+		botaoSomar.addActionListener(this);
+		add(botaoSomar);
+
+		botaoSubtrair = new JButton("-");
+		botaoSubtrair.addActionListener(this);
+		add(botaoSubtrair);
+
+		botaoMultiplicar = new JButton("*");
+		botaoMultiplicar.addActionListener(this);
+		add(botaoMultiplicar);
+
+		botaoDividir = new JButton("/");
+		botaoDividir.addActionListener(this);
+		add(botaoDividir);
+
+		botaoIgual = new JButton("=");
+		botaoIgual.addActionListener(this);
+		add(botaoIgual);
+
 		botaoLimpar = new JButton("C");
 		botaoLimpar.addActionListener(this);
-		add(botaoLimpar);		
+		add(botaoLimpar);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {				
-	}
+	public void actionPerformed(ActionEvent e) {
 
+		if (txtText.getText().isEmpty()) {
+			return;
+		}
+
+		JButton botaoClicado = (JButton) e.getSource();
+
+		if (botaoClicado == botaoLimpar) {
+			// Como operacao é do tipo char não aceita "null", no char se quero definir valor
+			// nulo tenho que usar o código unicode abaixo.
+			operacao = '\u0000';
+			txtText.setText("");
+			
+		} else if (botaoClicado == botaoIgual) {
+
+			valor2 = Double.parseDouble(txtText.getText());
+
+			double resultado = 0.0;
+
+			if (operacao == '+') {
+				resultado = valor1 + valor2;
+
+			} else if (operacao == '-') {
+				resultado = valor1 - valor2;
+
+			} else if (operacao == '*') {
+				resultado = valor1 * valor2;
+
+			} else if (operacao == '/') {
+				resultado = valor1 / valor2;
+			}
+
+			txtText.setText(String.valueOf(resultado));
+			operacao = '\u0000';
+			valor1 = resultado;
+			valor2 = 0;
+			
+		} else {
+			operacao = botaoClicado.getText().charAt(0);
+			valor1 = Double.parseDouble(txtText.getText());
+			txtText.setText("");
+		}
+	}
 }
